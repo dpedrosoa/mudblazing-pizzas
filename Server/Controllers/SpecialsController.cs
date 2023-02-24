6e2d047470b74e9d89995fa704ff2877afef46d4
Dayana <dpedrosoa@gmail.com>
2023-02-24 12:54:29 -0500

    Add Specials Controller

---
+++ Server/Controllers/SpecialsController.cs
@@ -0,0 +1,26 @@
+﻿using BlazingPizzas.Server.Data.UnitOfWork;
+using BlazingPizzas.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazingPizzas.Server.Controllers
+{
+    [Route("api/specials")]
+    [ApiController]
+    public class SpecialsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SpecialsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PizzaSpecial>>> Get()
+        {
+            var specials = await _unitOfWork.SpecialsRepository.GetAll();
+            return Ok(specials);
+        }
+    }
+}
